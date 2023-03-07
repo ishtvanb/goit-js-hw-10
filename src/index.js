7633eab540cb9babaf2c12e07fcc9f6c1121e34e
ishtvanb <110969832+ishtvanb@users.noreply.github.com>
2023-03-07 21:03:47 +0200

    Зміни до ДЗ10

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/styles.css';
-import { fetchCountries } from './js/fetchCountries/';
+import { fetchCountries } from './js/fetchCountries';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 
