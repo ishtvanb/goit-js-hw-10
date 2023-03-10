71cd4b2ffc4ff94d9a52357a2f5e1417d6ae7f6f
ishtvanb <110969832+ishtvanb@users.noreply.github.com>
2023-03-10 11:32:13 +0200

    Зміни до ДЗ10

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ function renderOneCountry(countries) {
     const markup = countries
     .map(country => {
             return
-        `<li><img src="${country.flags.svg}" alt="Flag of ${country.name.official}">
+        `<li><img src="${country.flags.svg}" alt="Flag of ${country.name.official}" width="30" high="20">
 <p>${country.name.official}</p>
 <p><b>Capital</b>: ${country.capital}</p>
 <p><b>Population</b>: ${country.population}</p>
